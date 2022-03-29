5608148b287c1a5e33f799ec4c7399b3662591ba
Michael Sheng <m3sheng@uwaterloo.ca>
2022-03-28 21:34:32 -0400

    Time: 0 ms (100.00%), Space: 54.3 MB (54.20%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        int pivot = (nums.length-1) / 2;
+        
+        
+        while(left <= right){
+            if(nums[pivot] > target){
+                right = pivot - 1;
+            }else if (nums[pivot] < target){
+                left = pivot + 1;
+            }else if (nums[pivot] == target){
+                return pivot;
+            }
+            
+            pivot = (right + left)/2;
+        }
+        return -1;
+    }
+}
