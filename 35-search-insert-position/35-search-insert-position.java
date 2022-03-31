8292f810415031d6328c96808530d16c5e7e494f
Michael Sheng <m3sheng@uwaterloo.ca>
2022-03-31 19:51:23 -0400

    Time: 2 ms (9.82%), Space: 43.8 MB (30.75%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while(left <= right){
+            mid = (left + right)/2;
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else if (nums[mid] > target){
+                right = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        
+        System.out.println(mid);
+        
+        if (nums[mid] < target){
+            mid++;
+        }
+        return mid;
+   
+    }
+}
